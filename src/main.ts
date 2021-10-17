33b778e304a4bbe5e8cfa27e2cd6b27e22e56dfd
Konrad Duleba <kon.duleba@gmail.com>
2021-10-17 22:12:19 +0200

    changed import type

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import cors from 'cors'
+import * as cors from 'cors'
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
