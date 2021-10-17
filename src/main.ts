f7d44641c30a8061b42b0d50eb51f67d6b0f3329
Konrad Duleba <kon.duleba@gmail.com>
2021-10-17 22:05:30 +0200

    Enabled cors in maints

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cors from 'cors'
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
-  app.use(cookieParser());
+  // app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
+  app.use(cors())
+  // app.use(cookieParser());
 
   app.useStaticAssets(join(__dirname, '..', 'src/public/images'), {
     prefix: '/photo',
